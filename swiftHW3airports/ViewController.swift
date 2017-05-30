3ff3fe0a36e8c67c994ff3d922ae497e18bd4a4b
xu3gl4jo <xu3gl4jo@gmail.com>
2017-05-30 22:35:04 +0800

    2

--- swiftHW3airports/ViewController.swift
+++ swiftHW3airports/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    struct objects {
+        var sectionName : String!
+        var sectionObjects : [String]!
+        
+    }
+    
+    var objectArray = [objects]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        objectArray = [objects(sectionName:"section 1",sectionObjects:["a","b","c","d "])]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt index: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"Cell") as UITableViewCell!
+        
+        cell?.textLabel!.text = objectArray[index.section].sectionObjects[index.row]
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return objectArray[section].sectionObjects.count
+    }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return objectArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return objectArray[section].sectionName
+    }
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
