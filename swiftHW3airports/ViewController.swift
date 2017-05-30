6adaebb5d2217fd168307223b409d05c2067d501
xu3gl4jo <xu3gl4jo@gmail.com>
2017-05-30 19:33:45 +0800

    1

---
+++ swiftHW3airports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftHW3airports
+//
+//  Created by 李紹威 on 2017/5/30.
+//  Copyright © 2017年 李紹威. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
